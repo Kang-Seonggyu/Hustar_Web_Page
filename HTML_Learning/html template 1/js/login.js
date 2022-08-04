af48d2ac0829ce82cb68cb62974e95bba01810e6
Kang-Seonggyu <jokekim609@gmail.com>
2022-08-05 02:01:49 +0900

    로그인 JS
    이름 변경 : app.js -> login.js
    저장 정보를 바로 key:value 로 localstorage에 저장 하는 것이 아닌 배열과 객체 이용해서 여러 정보 저장 가능하도록 !
    수정해야할 사항 : Set 추가 함수
    추가해야할 사항 : Set 삭제 함수
                         비밀번호 변경함수

--- HTML_Learning/html template 1/js/login.js
+++ HTML_Learning/html template 1/js/login.js
@@ -1,8 +1,13 @@
-const checkID = document.querySelector("#inputID");
-const checkPW = document.querySelector("#inputPassword");
-const dcheckPW = document.querySelector("#inputPasswordConfirm");
+const checkID = document.querySelector("#inputID");             // 적은 ID
+const checkPW = document.querySelector("#inputPassword");       // 적은 PW
+const dcheckPW = document.querySelector("#inputPasswordConfirm"); // 적은 PW 재확인
+// 마스터 계정 : 관리자 ID 추가 만드는 계정
 const masterID = 'master';
 const masterPW = 'masterpw1234';
+
+let loadSet =[];
+
+//const inputID = JSON.parse(localStorage.getItem('loginKey'))['id']
 
 function onLoginSubmit(event){  //추후에 DB연결해서 관리자 테이블 값 가져와서 처리해야 함
     if(checkID.value == '' || checkPW.value == ''){
@@ -50,3 +55,27 @@ function makeAccountEnter(event){
     	makeAccount(event);
     }
 }
+
+function registerLoginSets (newID, newPW) {
+    // 로그인 셋 불러오기
+    if(localStorage.getItem('loginSets')) {
+        loadSet = JSON.parse(localStorage.getItem('loginSets'))
+        console.log(loadSet)
+        //loadSet
+    }
+    const ID_set = {'id': newID ,'pw': newPW};
+    loadSet.push(ID_set);
+    localStorage.setItem("loginSets",JSON.stringify(loadSet));
+}
+
+console.log(localStorage.getItem('loginSets'));
+registerLoginSets('checkID.value1', 'checkPW.value1');
+console.log(localStorage.getItem('loginSets'));
+registerLoginSets('checkID.value2', 'checkPW.value2');
+console.log(localStorage.getItem('loginSets'));
+registerLoginSets('checkID.value3', 'checkPW.value3');
+console.log(localStorage.getItem('loginSets'));
+//console.log('함수 통과 :',JSON.stringify(tempw));
+//console.dir(JSON.parse(localStorage.getItem('loginKey')))
+
+//console.log(JSON.parse(localStorage.getItem('loginSets')))
