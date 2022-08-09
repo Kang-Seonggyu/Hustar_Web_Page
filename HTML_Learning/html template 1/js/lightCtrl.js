8a401cdb1ce71349acd50387f8d603a9eff093af
Kang-Seonggyu <jokekim609@gmail.com>
2022-08-10 00:07:12 +0900

    Warning Log 틀 만들기
    warning 은 틀 잡음
    caution 도 만들면 좋을듯
    그리고 warning 과  caution 의 log시작에 해당 색의 동그라미 띄워주면 좋을듯.

--- HTML_Learning/html template 1/js/lightCtrl.js
+++ HTML_Learning/html template 1/js/lightCtrl.js
@@ -3,29 +3,31 @@ workersMount = localStorage.getItem("workersMount");
 
 function lightUpdate() {
     for (i = 0; i < workersMount*4; i++)  {
-    
+        // Index 값     : n번 째 사람
+        // no_lihgt 값  : n번 째 신호
         let index = parseInt(i/4);
         let no_light = parseInt(i%4+1);
-        // 정보를 변경할 신호 선택
+        // w_write : 정보를 변경할 신호 선택
         let w_write = document.getElementById(`w${i+1}`);
-        // 측정된 값 (w1 : 심박수, w2 : 혈압 , w3 : 혈중산소포화도 , w4 : 스트레스 )
+        // writeValue : 입력할 값
+        // (w1 : 심박수, w2 : 혈압 , w3 : 혈중산소포화도 , w4 : 스트레스 )
         let writeValue = 0;
         
         if (no_light == 1) {
             writeValue = workers[index].w1;
-            light1(writeValue, w_write)
+            light1(index, writeValue, w_write)
         }
         else if (no_light == 2) {
             writeValue = workers[index].w2;
-            light2(writeValue, w_write)
+            light2(index, writeValue, w_write)
         }
         else if (no_light == 3) {
             writeValue = workers[index].w3;
-            light3(writeValue, w_write)
+            light3(index, writeValue, w_write)
         }
         else {
             writeValue = workers[index].w4;
-            light4(writeValue, w_write)
+            light4(index, writeValue, w_write)
         }
     
         w_write.innerHTML = writeValue;
@@ -33,9 +35,10 @@ function lightUpdate() {
 }
 
 //심박수 신호 제어
-function light1(heartRate, WantToChangeSingal) {
+function light1(index, heartRate, WantToChangeSingal) {
     if (heartRate>=150) {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(1,index,heartRate)
     }
     else if (heartRate>=120) {
         WantToChangeSingal.style.background = 'orange';
@@ -48,14 +51,16 @@ function light1(heartRate, WantToChangeSingal) {
     }
     else {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(1,index,heartRate)
     }
     
 }
 
 // 혈압 신호 제어
-function light2(BloodPressure, WantToChangeSingal) {
+function light2(index, BloodPressure, WantToChangeSingal) {
     if (BloodPressure>=140) {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(2,index,BloodPressure)
     }
     else if (BloodPressure>=120) {
         WantToChangeSingal.style.background = 'orange';
@@ -68,11 +73,12 @@ function light2(BloodPressure, WantToChangeSingal) {
     }
     else {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(2,index,BloodPressure)
     }   
 }
 
 // 혈중산소포화도 신호 제어
-function light3(O2, WantToChangeSingal) {
+function light3(index, O2, WantToChangeSingal) {
     if (O2>=80) {
         WantToChangeSingal.style.background = 'rgb(16, 242, 16)';
     }
@@ -81,19 +87,22 @@ function light3(O2, WantToChangeSingal) {
     }
     else {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(3,index,O2)
     } 
 }
 
 // 스트레스 신호 제어
-function light4(Stresses, WantToChangeSingal) {
-    if (Stresses>=80) {
+function light4(index, Stress, WantToChangeSingal) {
+    if (Stress>=80) {
         WantToChangeSingal.style.background = 'red';
+        warning_Write(4,index,Stress)
     }
-    else if (Stresses>=60) {
+    else if (Stress>=60) {
         WantToChangeSingal.style.background = 'orange';
     }
     else {
         WantToChangeSingal.style.background = 'rgb(16, 242, 16)';
+        
     } 
 }
 
